ec4e747d313ab6ffa1d3d14d0ae5e9fd4201d262
Yang Seungwon <gavrakr@naver.com>
2024-10-01 02:20:23 +0900

    2024_10_01_02:20

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tweet, Link
+from .models import Tweet, Like
 
 
 @admin.register(Tweet)
@@ -7,13 +7,14 @@ class TweetAdmin(admin.ModelAdmin):
     list_display = (
         "payload",
         "user",
+        "total_like",
         "created_at",
         "updated_at",
     )
 
 
-@admin.register(Link)
-class LinkAdmin(admin.ModelAdmin):
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
     list_display = (
         "user",
         "tweet",
--- tweets/models.py
+++ tweets/models.py
@@ -11,20 +11,23 @@ class Tweet(CommonModel):
         related_name="tweets",
     )
 
+    def total_like(self):
+        return self.likes.count()
+
     def __str__(self):
         return self.payload
 
 
-class Link(CommonModel):
+class Like(CommonModel):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="tweets_links",
+        related_name="likes",
     )
     tweet = models.ForeignKey(
         Tweet,
         on_delete=models.SET_NULL,
-        related_name="tweets_links",
+        related_name="likes",
         null=True,
     )
 
