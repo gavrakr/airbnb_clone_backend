f69fb42eb3ccf2d756d6ecb8e500fd18d0953bdc
Yang Seungwon <gavrakr@naver.com>
2024-10-01 01:01:21 +0900

    2024_10_01_01:01

--- direct_messages/models.py
+++ direct_messages/models.py
@@ -7,6 +7,7 @@ class ChattingRoom(CommonModel):
 
     users = models.ManyToManyField(
         "users.User",
+        related_name="direct_messages",
     )
 
     def __str__(self):
@@ -22,10 +23,12 @@ class Message(CommonModel):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
+        related_name="direct_messages",
     )
     room = models.ForeignKey(
         "direct_messages.ChattingRoom",
         on_delete=models.CASCADE,
+        related_name="direct_messages",
     )
 
     def __str__(self):
