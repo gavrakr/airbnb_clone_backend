d5b3924fe4601c68acc82c0c6a34f1e181734f4e
Yang Seungwon <gavrakr@naver.com>
2024-10-03 01:55:52 +0900

    2024_10_03_01:55

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.all_tweets),
+    path("", views.tweets_board),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Tweet
 
 
-def all_tweets(request):
+def tweets_board(request):
     tweets = Tweet.objects.all()
     return render(
         request,
         "tweet.html",
         {
-            "title": "Tweet Board",
+            "title": "Tweet Main Page",
             "tweets": tweets,
         },
     )
