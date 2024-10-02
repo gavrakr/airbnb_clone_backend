7a27bdc6160d97c74ac5f9c8bb715eeba3344892
Yang Seungwon <gavrakr@naver.com>
2024-10-03 00:46:23 +0900

    2024_10_03_00:46

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def see_all_room(request):
+    return HttpResponse("see all rooms!")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"see room with id {room_id}")
